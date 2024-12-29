4615499952c875064f1de51dadf9c2779f1009ed
aaryanshukla <shuklaaaryan1@gmail.com>
2024-12-28 23:49:09 -0800

    connected to MongoDb

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require("mongodb");
+
+// Replace <cluster-url> with your cluster's host, e.g., "cluster0.mongodb.net"
+// Ensure special characters in the password are URL-encoded. For example, "!" becomes "%21".
+const uri = "mongodb+srv://aaryan:Curry1234%21@posedetection.zplo2.mongodb.net/?retryWrites=true&writeConcern=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+    
+
+    const database= client.db("Users");
+
+    const collection = database.collection("user_db");
+
+    const doc = { name: "Aaryan Shukla", posture: "curved"};
+
+    const result = await collection.insertOne(doc);
+
+
+    console.log(
+        `A document was inserted with the _id: ${result.insertedId}`,
+     );
+
+
+
+  } catch (err) {
+    console.error("An error occurred while connecting to MongoDB:", err);
+  } finally {
+    await client.close();
+    console.log("Connection closed.");
+  }
+}
+run().catch(console.dir);
